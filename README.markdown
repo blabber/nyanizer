68599c50dd5e31273f8f129b81494811018d1204
Tobias Rehbein <tobias.rehbein@web.de>
2012-03-16 23:59:11 +0100

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,26 @@
+nyanizer
+========
+
+Description
+-----------
+
+This is a small tool to nyanize pictures. It uses the MagickWand API for
+ImageMagick to read a picture and then iterates the pixels of the picture. The
+saturation of every pixel is raised to the maximum and the hue is increased
+every row in a way that you get a full color cycle over the whole image.
+
+Example
+-------
+
+![cat](./example/cat.jpg "cat")
+![nyaniced cat](./example/nyancat.jpg "nyanized cat")
+
+License
+-------
+
+The example image is taken from
+[here](https://de.wikipedia.org/w/index.php?title=Datei:Burma_creme.JPG).  It
+has been published by Linda Andersson under the [CC BY-SA 3.0
+License](https://creativecommons.org/licenses/by-sa/3.0/).
+
+The nyanizer itself is licensed under the beerware license (see the header of the source).
---
+++ src/nyanizer.c
@@ -0,0 +1,88 @@
+#include <err.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <math.h>
+#include <assert.h>
+#include <wand/MagickWand.h>
+
+void usage(void);
+
+int
+main(int argc, char **argv)
+{
+	MagickWandGenesis();
+
+	char *infile = NULL, *outfile = NULL;
+	int times = 1;
+
+	int opt;
+	while ((opt = getopt(argc, argv, "ht:i:o:")) != -1) {
+		switch (opt) {
+			case 't':
+				times = strtol(optarg, (char **)NULL, 10);
+				if (times <= 0)
+					times = 1;
+				break;
+			case 'i':
+				infile = strdup(optarg);
+				break;
+			case 'o':
+				outfile = strdup(optarg);
+				break;
+			case 'h':
+			default:
+				usage();
+		}
+	}
+
+	if (infile == NULL || outfile == NULL)
+		usage();
+
+	MagickWand *wand = NewMagickWand();
+	if (MagickReadImage(wand, infile) == MagickFalse)
+		err(EXIT_FAILURE, "could not read image: %s\n", infile);
+
+	PixelIterator *iterator = NewPixelIterator(wand);
+	if (iterator == NULL)
+		err(EXIT_FAILURE, "could not create PixelIterator");
+
+	size_t height = MagickGetImageHeight(wand);
+	double dhue = 1.0 / height * times;
+	double nhue = 0;
+
+	for (size_t y = 0; y < height; y++) {
+		size_t width;
+		PixelWand **pixels = PixelGetNextIteratorRow(iterator, &width);
+		if (pixels == NULL)
+			err(EXIT_FAILURE, "could not PixelGetNextIteratorRow");;
+
+		for (size_t x = 0; x < width; x++) {
+			double hue, sat, lightness;
+
+			PixelGetHSL(pixels[x], &hue, &sat, &lightness);
+			PixelSetHSL(pixels[x], nhue, 1, lightness);
+		}
+
+		nhue += dhue;
+		if (nhue >= 1)
+			nhue = 0;
+
+		PixelSyncIterator(iterator);
+	}
+
+	MagickWriteImages(wand, outfile , MagickTrue);
+
+	DestroyPixelIterator(iterator);
+	DestroyMagickWand(wand);
+	MagickWandTerminus();
+
+	return (EXIT_SUCCESS);
+}
+
+void usage(void)
+{
+	printf("usage: %s [-t times] -i <infile> -o <outfile>\n", getprogname());
+	exit(1);
+}
